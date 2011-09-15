bf60e8671ac95080244092aea8bb9fab74485e33
Paul Michael Bauer <paul.michael.bauer@gmail.com>
2011-09-15 01:10:28 -0700

    code beautification

--- src/main/clojure/alioth/mandelbrot.clj
+++ src/main/clojure/alioth/mandelbrot.clj
@@ -47,48 +47,47 @@
            zr1 crbx+i   zi1 ciby
            zr2 crbx+i+1 zi2 ciby 
            b 0]
-          (if (< j MAX_ITERATIONS)
-            (let [nzr1 (+ crbx+i   (- (* zr1 zr1) (* zi1 zi1))) 
-                  nzi1 (+ ciby (* zr1 zi1 2))
-                  nzr2 (+ crbx+i+1 (- (* zr2 zr2) (* zi2 zi2)))
-                  nzi2 (+ ciby (* zr2 zi2 2))
-                  nb (if (> (+ (* nzr1 nzr1) (* nzi1 nzi1)) 4) (bit-or b 2) b)
-                  nb (if (> (+ (* nzr2 nzr2) (* nzi2 nzi2)) 4) (bit-or nb 1) nb)]
-              (if (= nb 3)
-                nb
-                (recur (inc j) nzr1 nzi1 nzr2 nzi2 nb)))
-            b))))
+      (if (< j MAX_ITERATIONS)
+        (let [nzr1 (+ crbx+i   (- (* zr1 zr1) (* zi1 zi1))) 
+              nzi1 (+ ciby (* zr1 zi1 2))
+              nzr2 (+ crbx+i+1 (- (* zr2 zr2) (* zi2 zi2)))
+              nzi2 (+ ciby (* zr2 zi2 2))
+              nb (if (> (+ (* nzr1 nzr1) (* nzi1 nzi1)) 4) (bit-or b 2) b)
+              nb (if (> (+ (* nzr2 nzr2) (* nzi2 nzi2)) 4) (bit-or nb 1) nb)]
+          (if (= nb 3)
+            nb
+            (recur (inc j) nzr1 nzi1 nzr2 nzi2 nb)))
+        b))))
 
 (defn get-byte ^long [^long x ^long y ^CXB cxb]
   (loop [i 0 res 0]
-      (if (< i 8)
-        (recur (+ i 2) (+ (bit-shift-left res 2)
-                          (get-byte-inner x y i cxb)))
-        (bit-xor res -1))))
+    (if (< i 8)
+      (recur (+ i 2) (+ (bit-shift-left res 2)
+                        (get-byte-inner x y i cxb)))
+      (bit-xor res -1))))
 
 (defn put-line [^bytes out ^long m ^long y ^CXB cxb]
   (let [offset (* y m)]
-      (for-loop [(xb 0) (< xb m) (inc xb)]
-        (aset out (+ offset xb) (byte (get-byte (* 8 xb) y cxb))))))
+    (for-loop [(xb 0) (< xb m) (inc xb)]
+      (aset out (+ offset xb) (byte (get-byte (* 8 xb) y cxb))))))
 
 (defn ^MandelbrotBuffer compute-mandelbrot [^long n]
   (let [m (/ (+ n 7) 8)
         ^bytes out (byte-array (* n m))
         ^CXB cxb (make-cxb n)
         yIdx (atom -1)
-        runner (reify Runnable
-                 (run [_] (let [y (swap! yIdx inc)]
-                            (if (< y n) (do (put-line out m y cxb) (recur))))))
-        pool (map (fn [_] (Thread. runner)) (range (* 2 (.availableProcessors (Runtime/getRuntime)))))]
-    (doseq [^Thread thread pool]
-      (.start thread)) 
-    (doseq [^Thread thread pool]
-      (.join thread))
+        runner (reify Runnable (run [_] (let [y (swap! yIdx inc)]
+                                          (if (< y n)
+                                            (do (put-line out m y cxb)
+                                                (recur))))))
+        pool (map (fn [_] (Thread. runner))
+                  (range (* 2 (.. Runtime getRuntime availableProcessors))))]
+    (doseq [^Thread thread pool] (.start thread)) 
+    (doseq [^Thread thread pool] (.join thread))
     (MandelbrotBuffer. out n m)))
 
 (defn write-bmp [^OutputStream outStream ^MandelbrotBuffer buff]
-  (let [^bytes out (.out buff) n (.n buff) m (.m buff)
-        len (* n m)] ; (.length out) <-- won't compile ??
+  (let [^bytes out (.out buff) n (.n buff) m (.m buff) len (* n m)] ; (.length out) <-- won't compile ??
     (.write outStream (.getBytes (str "P4\n" n " " n "\n"))) 
     (.write outStream out 0 len) ; puzzling bug, just (.write outStream out) prints garbage
     outStream))
