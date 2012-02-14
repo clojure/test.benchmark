5fa1d42ac2a11f38ab560c4998f56fd42aa9d195
Paul M Bauer <paul.michael.bauer@gmail.com>
2012-02-13 18:17:00 -0700

    Initial regexdna checkin

---
+++ src/main/clojure/alioth/regexdna.clj
@@ -0,0 +1,133 @@
+;   Copyright (c) Rich Hickey and contributors.
+;   All rights reserved.
+;   The use and distribution terms for this software are covered by the
+;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;   which can be found in the file epl-v10.html at the root of this distribution.
+;   By using this software in any fashion, you are agreeing to be bound by
+;   the terms of this license.
+;   You must not remove this notice, or any other, from this software.
+;
+;   Alioth benchmarks: http://shootout.alioth.debian.org/u64q/benchmark.php?test=regexdna
+(ns alioth.regexdna
+  (:import [java.io FileDescriptor FileInputStream])
+  (:import [java.nio ByteBuffer])
+  (:import [java.nio.channels FileChannel])
+  (:import [java.util.regex Matcher Pattern])
+  (:gen-class))
+
+(set! *warn-on-reflection* true)
+(set! *unchecked-math* true)
+
+(definterface LengthSetable
+  (^void setLength [^long l]))
+
+(deftype ByteWrapper [^bytes backing ^long ^:unsynchronized-mutable length]
+  CharSequence
+    (charAt [_ index] (char (aget backing index)))
+    (length [_] length)
+  LengthSetable
+    (^void setLength [_ ^long l] (set! length l)))
+
+(def ^:const comments (Pattern/compile ">.*\n|\n"))
+(def ^:const codes-pat (Pattern/compile "[BDHKMNRSVWY]"))
+
+(def codes [["B" "(c|g|t)"]
+            ["D" "(a|g|t)"]
+            ["H" "(a|c|t)"]
+            ["K" "(g|t)"]
+            ["M" "(a|c)"]
+            ["N" "(a|c|g|t)"]
+            ["R" "(a|g)"]
+            ["S", "(c|g)"]
+            ["V", "(a|c|g)"]
+            ["W", "(a|t)"]
+            ["Y", "(c|t)"]])
+(def longest (long (reduce #(max %1 (count (second %2))) 0 codes)))
+
+(def _strs ["agggtaaa|tttaccct"
+            "[cgt]gggtaaa|tttaccc[acg]"
+            "a[act]ggtaaa|tttacc[agt]t"
+            "ag[act]gtaaa|tttac[agt]ct"
+            "agg[act]taaa|ttta[agt]cct"
+            "aggg[acg]aaa|ttt[cgt]ccct"
+            "agggt[cgt]aa|tt[acg]accct"
+            "agggta[cgt]a|t[acg]taccct"
+            "agggtaa[cgt]|[acg]ttaccct"
+            ])
+(def ^"[Ljava.lang.Object;" strs (into-array _strs))
+(def ^"[Ljava.lang.Object;" pats (into-array (map #(Pattern/compile %) _strs)))
+
+(def repl
+  (let [buf (byte-array (* 26 (inc longest)))]
+    (doseq [[offset ^String code] (map (fn [[^String l code]]
+                                         [(* longest (- (int  (.charAt l 0)) (int \A))) code])
+                                       codes)]
+      (doseq [j (range (count code))]
+        (aset buf (+ offset j) (byte (int (.charAt code j))))))
+    buf))
+
+(defn rm-comments [^ByteWrapper t]
+  (let [^bytes backing (.backing t)
+        backing-length (count backing) ; todo check if needed
+        m (.matcher comments t)]
+    (if (.find m)
+      (.setLength t (loop [tail (.start m), restart (.end m)]
+                      (if (.find m)
+                        (recur (long (loop [tail tail, restart restart]
+                                       (if (not= restart (.start m))
+                                         (do (aset backing tail (aget backing restart))
+                                             (recur (inc tail) (inc restart)))
+                                         tail)))
+                               (.end m))
+                        (loop [tail tail, restart restart]
+                          (if (< restart backing-length)
+                            (do (aset backing tail (aget backing restart))
+                                (recur (inc tail) (inc restart)))
+                            tail))))))))
+
+(defn count-patterns [^ByteWrapper t]
+  (loop [i 0]
+    (when (< i (count pats))
+      (let [m (.matcher ^Pattern (aget pats i) t)
+            c (loop [c 0] (if (.find m) (recur (inc c)) c))]
+        (println (aget strs i) c))
+      (recur (inc i)))))
+
+(defn replace-in ^ByteWrapper [^ByteWrapper t]
+  (let [^bytes backing (.backing t)
+        ^bytes repl repl
+        buf (byte-array (* (.length t) longest))
+        m (.matcher codes-pat t)
+        pos (loop [pos 0, last 0]
+              (if (.find m)
+                (let [[p l] (loop [pos pos, last last]
+                              (if (< last (.start m))
+                                (do (aset buf pos (aget backing last))
+                                    (recur (inc pos) (inc last)))
+                                (loop [pos pos, i (* longest (- (int (aget backing last)) (int \A)))]
+                                  (if (not= 0 (int (aget repl i)))
+                                    (do (aset buf pos (aget repl i))
+                                        (recur (inc pos) (inc i)))
+                                    [pos (inc last)]))))]
+                  (recur (long p) (long l)))
+                (loop [pos pos, last last]
+                  (if (< last (.length t))
+                    (do (aset buf pos (aget backing last))
+                        (recur (inc pos) (inc last)))
+                    pos))))]
+    (ByteWrapper. buf pos)))
+
+(defn regexdna [^bytes ba]
+  (let [t (ByteWrapper. ba (count ba))]
+    (rm-comments t)
+    (count-patterns t)
+    (println)
+    (println (count (.backing t)))
+    (println (.length t))
+    (println (.length (replace-in t)))))
+
+(defn -main [& args]
+  (let [cin (-> FileDescriptor/in FileInputStream. .getChannel)
+        bb (-> cin .size int ByteBuffer/allocate)]
+    (.read cin bb)
+    (regexdna (.array bb))))
---
+++ src/test/clojure/test/alioth/regexdna.clj
@@ -0,0 +1,49 @@
+;   Copyright (c) Rich Hickey and contributors.
+;   All rights reserved.
+;   The use and distribution terms for this software are covered by the
+;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;   which can be found in the file epl-v10.html at the root of this distribution.
+;   By using this software in any fashion, you are agreeing to be bound by
+;   the terms of this license.
+;   You must not remove this notice, or any other, from this software.
+(ns test.alioth.regexdna
+  (:use [alioth.regexdna]
+        [alioth.util.core]
+        [clojure.java.io :only [input-stream]]
+        [clojure.test])
+  (:import [java.nio ByteBuffer])
+  (:import [java.io InputStream
+                    ByteArrayOutputStream
+                    ByteArrayInputStream
+                    OutputStreamWriter]))
+
+(def ^:private this-ns *ns*) ; work around bug in com.theoryinpractice.clojure.testrunner 
+(defn- resource-stream [file-name]
+  (input-stream (ns-resource this-ns file-name)))
+
+(defn- to-byte-array ^bytes [^InputStream input]
+  (let [out (ByteArrayOutputStream.)
+        buffer-size 16384
+        buffer (byte-array buffer-size)
+        read #(.read input buffer 0 buffer-size)]
+    (loop [readc (read)]
+      (when (not= readc -1)
+        (.write out buffer 0 readc)
+        (recur (read))))
+    (.flush out)
+    (.toByteArray out)))
+
+(defn- regexdna-on-input-matches-expected [in-file-name expected-file-name]
+  (with-open [out (ByteArrayOutputStream.)
+              input (resource-stream in-file-name)]
+    (binding [*out* (OutputStreamWriter. out)]
+      (regexdna (to-byte-array input)))
+    (with-open [output (ByteArrayInputStream. (.toByteArray out))
+                expected (resource-stream expected-file-name)]
+      (streams= expected output))))
+
+(deftest test-regexdna-correctness
+  (are [in            expected] (regexdna-on-input-matches-expected in expected)
+       "fasta100.txt"   "expected100.txt"
+       "fasta10000.txt" "expected10000.txt"
+       ))
