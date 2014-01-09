eec4ab391fa9539231e276f667e3003f51ac2bdb
Alex Miller <alex@puredanger.com>
2014-01-09 10:43:14 -0600

    updated binarytrees impl submitted to alioth

--- src/main/clojure/alioth/binary_trees.clj
+++ src/main/clojure/alioth/binary_trees.clj
@@ -1,55 +1,54 @@
-;   Copyright (c) Rich Hickey and contributors.
-;   All rights reserved.
-;   The use and distribution terms for this software are covered by the
-;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
-;   which can be found in the file epl-v10.html at the root of this distribution.
-;   By using this software in any fashion, you are agreeing to be bound by
-;   the terms of this license.
-;   You must not remove this notice, or any other, from this software.
+;; The Computer Language Benchmarks Game
+;; http://benchmarksgame.alioth.debian.org/
 
-;   Alioth benchmarks: http://shootout.alioth.debian.org/u64q/benchmark.php?test=binarytrees&lang=all
-;   Inspired by http://shootout.alioth.debian.org/u64q/program.php?test=binarytrees&lang=java&id=1
-;           and http://shootout.alioth.debian.org/u64q/program.php?test=binarytrees&lang=clojure&id=5                                        ;
-(ns alioth.binary-trees
+;; Adapted from the Java -server version
+;; contributed by Marko Kocic
+;; modified by Kenneth Jonsson, restructured to allow usage of 'pmap'
+;; modified by Andy Fingerhut to use faster primitive math ops, and
+;; deftype instead of defrecord for smaller tree nodes.
+;; modified by Rich Hickey for Clojure 1.3
+;; modified promise/delivery improvement by Stuart Halloway
+;; small hints by David Nolen and Alex Miller
+                                        ;
+(ns alioth.binary-trees-me
   (:gen-class))
 
 (set! *warn-on-reflection* true)
 (set! *unchecked-math* true)
 
-(def ^:const min-depth 4)
+(def ^:const ^long min-depth 4)
 
-(deftype TreeNode [left right ^int item])
+(deftype TreeNode [left right ^long item])
 
 (defn make-tree [^long item ^long depth]
   (if (zero? depth)
     (TreeNode. nil nil item)
-    (TreeNode.
-     (make-tree (dec (* 2 item)) (dec depth))
-     (make-tree (* 2 item) (dec depth))
-     item)))
+    (let [i2 (* 2 item)
+          ddec (dec depth)]
+      (TreeNode. (make-tree (dec i2) ddec) (make-tree i2 ddec) item))))
 
 (defn item-check ^long [^TreeNode node]
   (if (nil? (.left node))
     (.item node)
-    (+ (+ (.item node)
+    (- (+ (.item node)
           (item-check (.left node)))
-       (- (item-check (.right node))))))
+       (item-check (.right node)))))
 
-(defn iterate-trees [mx mn d]
+(defn iterate-trees [^long mx ^long mn ^long d]
   (let [iterations (bit-shift-left 1 (long (+ mx mn (- d))))]
     (format "%d\t trees of depth %d\t check: %d"
             (* 2 iterations)
             d
             (loop [result 0
-                   i 1]
-              (if (= i (inc iterations))
-                result
-                (recur (+ result
-                          (item-check (make-tree i d))
-                          (item-check (make-tree (- i) d)))
-                       (inc i)))))))
+         i 1]
+    (if (= i (inc iterations))
+      result
+      (recur (+ result
+                (item-check (make-tree i d))
+                (item-check (make-tree (- i) d)))
+             (inc i)))))))
 
-(defn main [max-depth]
+(defn main [^long max-depth]
   (let [stretch-depth (inc max-depth)]
     (let [tree (make-tree 0 stretch-depth)
           check (item-check tree)]
@@ -68,7 +67,7 @@
         (println (format "long lived tree of depth %d\t check: %d" max-depth (item-check long-lived-tree))))))
 
 (defn -main [& args]
-  (let [n (if (first args) (Integer/parseInt (first args)) 0)
+  (let [n (if (first args) (Long/parseLong (first args)) 0)
         max-depth (if (> (+ min-depth 2) n) (+ min-depth 2) n)]
     (main max-depth)
     (shutdown-agents)))
